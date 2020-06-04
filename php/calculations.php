0a304280faa2fbd2cb6da0cf8386a598230ceff7
Dave Bennett <dnbennett@hotmail.co.uk>
2020-06-04 17:03:55 +0100

    CSV data filters and displayed

---
+++ php/calculations.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+ <?php
+    // Open csv file, create empty array for data and get csv headers
+    $file = fopen("calculations.csv", "r");
+    $csvArray = array();
+    $header = fgetcsv($file);
+
+    // Loop through csv file and create associative array with the headers as keys
+    while ($row = fgetcsv($file)) {
+        $csvArray[] = array_combine($header, $row);
+    }
+
+    // Sort array based on 'Timestamp' column
+    $time = array();
+    foreach($csvArray as $key => $row) {
+        $time[$key] = $row['Timestamp'];
+    }
+    array_multisort($time, SORT_DESC, $csvArray);
+
+    // Create HTML table
+    echo "<table><tr>";
+
+    // Add headers as table headers
+    foreach($header as $cell) {
+        echo "<th>" . htmlspecialchars($cell) . "</th>";
+    }
+
+    echo "</tr>";
+
+    // Loop through data array and add each element as a row in the table
+    foreach($csvArray as $arr) {
+        echo "<tr>";
+        foreach($arr as $key => $value) {
+            echo "<td>" . htmlspecialchars($value) . "</td>";
+        }
+        echo "</tr>";
+    }
+
+    echo "</table>";
+    ?>
+ </body>
+</html>
--- php/storeData.php
+++ php/storeData.php
@@ -1,29 +1,16 @@
 <?php
     $calc = $_GET['calc'];
     $calc = str_replace(' ', '+', $calc);
-    print_r($calc);
-
-    print_r("\n");
-
     $ip = $_SERVER['REMOTE_ADDR'];
-    print_r($ip);
-
-    print_r("\n");
-
-    $dateAdded = date("h:i:sa d/m/y");
-    print_r($dateAdded);
-
-    print_r("\n");
-
+    $timestamp = date('Y-m-d H:i:s');
     $browser = $_SERVER['HTTP_USER_AGENT'];
-    print_r($browser);
 
     // Open file for writing
     $file = fopen('calculations.csv', 'a');
     
     // Put data into array
     $data = array(
-        $calc, $ip, $dateAdded, $browser
+        $calc, $ip, $timestamp, $browser
     );
     
     // Save data to file
